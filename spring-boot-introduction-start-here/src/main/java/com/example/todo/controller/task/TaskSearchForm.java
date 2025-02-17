14b77141f2050c13a297e366087422afc32f5947
nanasinogonbei-493561 <snowwhitegood493561@gmail.com>
2025-02-17 17:41:15 +0900

    初めてのコミット

---
+++ spring-boot-introduction-start-here/src/main/java/com/example/todo/controller/task/TaskSearchForm.java
@@ -0,0 +1,27 @@
+package com.example.todo.controller.task;
+
+import com.example.todo.service.task.TaskSearchEntity;
+import com.example.todo.service.task.TaskStatus;
+
+import java.util.List;
+import java.util.Optional;
+
+public record TaskSearchForm(
+        String summary,
+        List<String> status
+) {
+
+    public TaskSearchEntity toEntity(){
+        //        searchForm.status == null: List.of()
+        //        searchForm.status != null: List<TaskStatus>
+        var statusEntityList = Optional.ofNullable(status())
+                .map(statusList -> statusList.stream().map(TaskStatus::valueOf).toList())
+                .orElse(List.of());
+
+        return new TaskSearchEntity(summary(), statusEntityList);
+    }
+
+    public TaskSearchDTO toDTO() {
+        return new TaskSearchDTO(summary(), status());
+    }
+}
