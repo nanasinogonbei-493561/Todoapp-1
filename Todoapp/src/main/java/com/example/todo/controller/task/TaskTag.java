a89e905c5e1cbe02b353f8f0826824f662452606
nanasinogonbei-493561 <snowwhitegood493561@gmail.com>
2025-04-04 19:59:26 +0900

    Tag関連削除しました。

--- Todoapp/src/main/java/com/example/todo/controller/task/TaskTag.java
+++
@@ -1,4 +0,0 @@
-package com.example.todo.controller.task;
-
-public record TaskTag(String name) {
-}
--- Todoapp/src/main/java/com/example/todo/controller/task/TodoTag.java
+++
@@ -1,6 +0,0 @@
-package com.example.todo.controller.task;
-
-import java.util.List;
-
-public record TodoTag(String title, boolean completed, List<Tag> tags) {
-}
